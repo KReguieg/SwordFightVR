1ff7216b2b4bf8359a4a62486922ebb4f66c63df
U-holodeck1\artcom <holodeck@artcom.de>
2016-11-24 17:19:32 +0100

    first scene

---
+++ SwordFightVR/Assets/MyNetworkDiscovery.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetworkDiscovery : NetworkDiscovery {
+
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        base.OnReceivedBroadcast(fromAddress, data);
+        // NetworkManager.singleton.
+    }
+}
